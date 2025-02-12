e9ece5c4ce04702c28a2ef1a6ed461c8a0ed7d30
brakesakes <zanecochran2008@gmail.com>
2025-02-12 13:23:29 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple script to show a popup when the "Learn More" button is clicked.
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert("More details coming soon! Stay tuned for product updates and information on how you can get involved.");
+});
